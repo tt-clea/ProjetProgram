0252f448e7c55f44352047636c3af5271eb55d81
annendjeng <116177917+annendjeng@users.noreply.github.com>
2023-11-17 16:48:30 +0100

    a

--- src/DateLocation.java
+++ src/DateLocation.java
@@ -1,19 +1,15 @@
-import java.io.Serializable;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
-import java.io.Serializable;
-
 
 /**
  * A class to represent the start and end date of a video rental.
  */
-public class DateLocation implements Serializable{
+public class DateLocation {
 
-    private static final long serialVersionUID = 4L;
-	private String dateDebut; // Start date of the rental
+    private String dateDebut; // Start date of the rental
     private String dateFin; // End date of the rental
     private static final int MAXIMUM_RENTAL_DURATION_DAYS = 30; // Maximum allowed rental duration in days
 
@@ -26,35 +22,46 @@ public class DateLocation implements Serializable{
         this.dateFin = dateFin;
     }
 
+    
+    /**
+     * Sets the start date of the rental with validation.
+     *
+     * @param aDateDebut The start date to set for the rental.
+     */
     public void setDateDebut(String aDateDebut) {
 
-        try {
+        try { // Check if the input start date is not null
             if (aDateDebut!=null)
-            {
+            { // Create a SimpleDateFormat with a specific date format
                 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+             // Set lenient to false to enforce strict date parsing
                 sdf.setLenient(false);
+             // Parse the input start date and set it to the instance variable dateDebut
                 Date datedebut = sdf.parse(this.dateDebut = aDateDebut);
+             // Parse the end date obtained from the getDateFin() method
                 Date datefin = sdf.parse(getDateFin());
+             // Check if the start date is not after today and the end date is not before the start date
                 boolean isDatedebutBeforeTodays = !datedebut.after(new Date());
                 boolean isDateAfterDateDebut=!datefin.before(datedebut);
+             // If both conditions are met, print the start date
                 if(isDatedebutBeforeTodays && isDateAfterDateDebut)
                 {
                     System.out.println("La date de debut :"+dateDebut);
                 }
-                else {
+                else { // If the end date is before the start date, print an error message
                     if(!isDateAfterDateDebut)
                     {
                         System.out.println("La date de fin ne peut être antérieure à la date de début.");
                     }
                     if(!isDatedebutBeforeTodays)
-                    {
+                    { // If the start date is before today, print an error message
                         System.out.println("La date de début ne peut être antérieure à aujourd'hui");
                     }
 
                 }
             }
 
-        } catch (ParseException e) {
+        } catch (ParseException e) {  // Throw a runtime exception in case of a parsing error
             throw new RuntimeException(e);
         }
     }
@@ -69,23 +76,28 @@ public class DateLocation implements Serializable{
     public void setDateFin(String aDateFin) {
 
         SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+     // Set lenient to false to enforce strict date parsing
         sdf.setLenient(false);
-        try{
+        try{ // Check if the input end date is not null
             if (aDateFin !=null)
-            {
+            { // Parse the start date obtained from the getDateDebut() method
                 Date datedebut=sdf.parse(getDateDebut());
+             // Parse the input end date and set it to the instance variable dateFin
                 Date datefin=sdf.parse(this.dateFin=aDateFin);
+             // Check if the end date is not before the start date
                 boolean isDateAfterDateDebut=!datefin.before(datedebut);
+             // If the end date is after the start date, print the end date
                 if (isDateAfterDateDebut)
                 {
                     System.out.println("La date de fin :"+dateFin);
                 }
                 else {
+                	// If the end date is before the start date, print an error message
                     System.out.println("L'erreur de date");
                 }
             }
 
-        } catch (ParseException e) {
+        } catch (ParseException e) { // Throw a runtime exception in case of a parsing error
             throw new RuntimeException(e);
         }
 
@@ -117,3 +129,5 @@ public class DateLocation implements Serializable{
                 '}';
     }
 }
+
+
--- src/Genre.java
+++ src/Genre.java
@@ -1,30 +1,85 @@
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.List;
 import java.util.HashMap;
-public class Genre {
+
+/**
+ * Represents a genre.
+ * Allows the creation and manipulation of genre objects.
+ */
+
+public class Genre implements Serializable{
+	private static final long serialVersionUID = 5L;
 	private String genreNom;
 	private List<Genre> subGenre;
+	
+	/**
+     * Constructor to initialize a genre with the specified name.
+     *
+     * @param genreNom The name of the genre.
+     */
 	public Genre(String genreNom) {
 		this.genreNom=genreNom;
 		this.subGenre=new ArrayList<>();
 	}
+	
+	
+	
+	/**
+     * Gets the name of the genre.
+     *
+     * @return The name of the genre.
+     */
 	public String getGenreNom() {
 		return genreNom;
 	}
+	
+	/**
+     * Gets the list of sub-genres.
+     *
+     * @return The list of sub-genres.
+     */
 	public List<Genre> getSubGenre() {
 		return subGenre;
 	}
+	
+	/**
+     * Adds a sub-genre to the list of sub-genres.
+     *
+     * @param sub The sub-genre to add.
+     */
 	public void addSubGenre(Genre sub)
 	{
 		subGenre.add(sub);
 	}
+	
+	/**
+     * Sets the name of the genre.
+     *
+     * @param genreNom The name to set.
+     */
 	public void setGenreNom(String genreNom) {
 		this.genreNom = genreNom;
 	}
+	
+	/**
+     * Sets the list of sub-genres.
+     *
+     * @param subGenre The list of sub-genres to set.
+     */
 	public void setSubGenre(List<Genre> subGenre) {
 		this.subGenre = subGenre;
 	}
+	
+	/**
+     * Checks if two genres are equal based on their attributes.
+     *
+     * @param o Object to compare.
+     * @return True if the genres are equal, false otherwise.
+     */
+	
+	
 	@Override
 	public boolean equals(Object o) {
 		if (this == o) return true;
@@ -32,10 +87,26 @@ public class Genre {
 		Genre genre = (Genre) o;
 		return Objects.equals(genreNom, genre.genreNom) && Objects.equals(subGenre, genre.subGenre);
 	}
+	
+	/**
+     * Generates a hash code for the genre based on its attributes.
+     *
+     * @return The hash code of the genre.
+     */
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(genreNom, subGenre);
 	}
+	
+	
+	/**
+     * Returns a string representation of the genre.
+     *
+     * @return A string containing the name of the genre and its list of sub-genres.
+     */
+	
+	
 	@Override
 	public String toString() {
 		return genreNom+":"+subGenre;
