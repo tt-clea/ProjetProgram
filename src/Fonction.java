00b6e51aa36325cf451844e956fec2d6ae55838e
tt-clea <doubleetliu@126.com>
2023-11-19 00:18:02 +0100

    tiantian 2023/11/19 final version

--- src/Fonction.java
+++ src/Fonction.java
@@ -25,7 +25,7 @@ public class Fonction {
 	public Fonction(BdConnector bd) {
 		this.bd=bd;
 		this.abonnes=abonnes;
-		this.evolues=new Evolues();
+		this.evolues=new Evolues(bd);
 		this.films = new Film();
 	}
 
